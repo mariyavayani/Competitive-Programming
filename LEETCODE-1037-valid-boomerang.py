c802afd50f39ba225e3281232cccaf69ba80bd75
mariyavayani <vayanimariya5@gmail.com>
2026-08-11 13:44:13 +0500

    Add isBoomerang function to validate points

---
+++ LEETCODE-1037-valid-boomerang.py
@@ -0,0 +1,21 @@
+# Link to problem: https://leetcode.com/problems/valid-boomerang/
+# Approach: Cross-multiply slope equality to check collinearity without division
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+# Solution: 
+class Solution(object):
+    def isBoomerang(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        x1=points[0][0]
+        y1=points[0][1]
+        x2=points[1][0]
+        y2=points[1][1]
+        x3=points[2][0]
+        y3=points[2][1]
+        if (y2-y1)*(x3-x2)==(y3-y2)*(x2-x1):
+            return False
+        return True
+        
